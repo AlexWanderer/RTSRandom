8e263d1d060a985b5638b7d86003e9563ee8f85f
Euphonux <zz1322@nyu.edu>
2016-08-09 17:39:57 -0400

    imported retro pixel pro, test scene 3 added to show the effects.

---
+++ Assets/Scripts/DebugManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugManager : MonoBehaviour
+{
+
+    public static DebugManager instance;
+
+	//Debug;
+	public Spining spiningPerformer; 
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyUp(KeyCode.R) && Input.GetKey(KeyCode.LeftShift)) {
+			spiningPerformer.Play();
+			Debug.Log("<color=red> Spining! </color>");
+		}
+    }
+}
---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsManager : MonoBehaviour
+{
+    public static SettingsManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+		DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
